51d384ba0a089f0fcdc421e33456a88018cd030f
SoroushAli <84461814+SoroushAli@users.noreply.github.com>
2021-06-06 05:27:03 -0400

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,33 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Soroush Alinaghian
+ */
+package base;
+/*
+Example Output
+
+What is the length of the room in feet? 15
+What is the width of the room in feet? 20
+You entered dimensions of 15 feet by 20 feet.
+The area is
+300 square feet
+27.871 square meters
+The formula for this conversion is m2 = f2 × 0.09290304
+
+
+Constraints
+
+Keep the calculations separate from the output.
+Use a constant to hold the conversion factor.
+
+Challenges
+
+Revise the program to ensure that inputs are entered as numeric values. Don’t allow the user to proceed if the value entered is not numeric.
+Create a new version of the program that allows you to choose feet or meters for your inputs.
+Implement this program as a GUI program that automatically updates the values when any value changes.
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
